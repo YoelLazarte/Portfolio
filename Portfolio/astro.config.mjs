f57499405a80019230996089f1fe01887b6b2dc6
Yoel Lazarte <150711966+YoelLazarte@users.noreply.github.com>
2025-08-25 23:45:15 -0300

    Initial commit - My portfolio

---
+++ Portfolio/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    fallback: {
+      es: 'en',
+    }
+  },
+});
